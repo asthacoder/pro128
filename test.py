917a853eeb61935e12a9a4514d6920239a94da36
Astha <70913929+asthacoder@users.noreply.github.com>
2021-12-25 00:29:00 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,69 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+import requests
+
+bright_stars_url = 'https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars'
+
+page = requests.get(bright_stars_url)
+
+soup = bs(page.text,'html.parser')
+
+star_table = soup.find('table')
+
+temp_list= []
+table_rows = star_table.find_all('tr')
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text.rstrip() for i in td]
+    temp_list.append(row)
+
+
+Star_names=[]
+Distance = [] 
+Mass = []
+Radius = []
+Lum=[]
+
+for i in range(1,len(temp_list)):
+    Star_names.append(temp_list[i][1])
+    Distance.append(temp_list[i][3])
+    Mass.append(temp_list[i][5])
+    Radius.append(temp_list[i][6])
+    Lum.append(temp_list[i][7])
+    
+df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,Lum)),columns=['Star_name','Distance','Mass','Radius','Luminosity'])
+print(df2)
+
+df2.to_csv('bright_stars.csv')
+
+def scrape_more_data(hyperlink):
+    try:
+        page = requests.get(hyperlink)
+        soup = BeautifulSoup(page.content, "html.parser")
+        temp_list = []
+        for tr_tag in soup.find_all("tr", attrs={"class": "fact_row"}):
+            td_tags = tr_tag.find_all("td")
+            for td_tag in td_tags:
+                try:
+                    temp_list.append(td_tag.find_all("div", attrs={"class": "value"})[0].contents[0])
+                except:
+                    temp_list.append("")
+        new_planet_data.append(temp_list)
+    except:
+        time.sleep(1)
+        scrape_more_data(hyperlink)
+scrape()
+for index, data in enumerate(planet_data):
+    scrape_more_data(data[5])
+    print(f"{index+1} page done 2")
+final_planet_data = []
+for index, data in enumerate(planet_data):
+    new_planet_data_element = new_planet_data[index]
+    new_planet_data_element = [elem.replace("\n", "") for elem in new_planet_data_element]
+    new_planet_data_element = new_planet_data_element[:7]
+    final_planet_data.append(data + new_planet_data_element)
+with open("final.csv", "w") as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerow(headers)
+        csvwriter.writerows(final_planet_data)
